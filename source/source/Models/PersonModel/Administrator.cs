d97fa7ee21bd83b4c51ee9618e7afc2490edae65
Le Tuan Thanh <tuanthanhlvq413@gmail.com>
2025-10-11 00:57:53 +0700

    Dang update coffeshop report admin

--- source/source/Models/PersonModel/Administrator.cs
+++ source/source/Models/PersonModel/Administrator.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace source.Models.PersonModel
 {
-    internal class Administrator
+    internal class Administrator : Person
     {
+        private string _Username;
+        private string _Password;
+        public string Username { get { return _Username; } set { _Username = value; } }
+        public string Password { get { return _Password; } set { _Password = value; } }    
+        public Administrator(): base(){ }
+        public Administrator (string fullName, string email, string phone, string username, string password) : base(fullName, email, phone)
+        {
+            this._Username = username;
+            this._Password = password;
+        }
     }
 }
--- source/source/Models/Report.cs
+++ source/source/Models/Report.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using source.Models.OrderModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,43 @@ using System.Threading.Tasks;
 
 namespace source.Models
 {
-    internal class Report
+    internal class Report : EntityBase
     {
+        private DateTime _ReportDate;
+        private decimal _TotalRevenue;
+        private int _TotalOrders;
+
+        public DateTime ReportDate { get { return this._ReportDate; } set { this._ReportDate = value; } }
+        public decimal TotalRevenue { get { return this._TotalRevenue; } set { this._TotalRevenue = value; } }
+        public int TotalOrders { get { return this._TotalOrders; } set { this._TotalOrders = value; } }
+
+        public Report() {
+            ReportDate = DateTime.Now;
+            TotalRevenue = 0;
+            TotalOrders = 0;
+        }
+        public Report(int id, DateTime reportDate, decimal totalRevenue, int totalOrders) : base(id)
+        {
+            ReportDate = reportDate;
+            TotalRevenue = totalRevenue;
+            TotalOrders = totalOrders;
+        }
+        ~Report() { }
+
+        public void Generate(List<Order> orders)
+        {
+            TotalOrders = orders.Count;
+
+            foreach (var order in orders)
+            {
+                TotalRevenue += order.Total();
+            }
+        }
+
+        // Xuất báo cáo ra console
+        public void Xuat()
+        {
+           
+        }
     }
 }
